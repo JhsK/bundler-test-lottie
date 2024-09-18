d19bfe068b68505181e2e41d9f7a1ce420ebaed1
JhsK <qwe6293@nate.com>
2024-09-18 18:49:45 +0900

    esm test

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,20 +4,20 @@ export default defineConfig({
   entry: ["src/index.ts"],
   splitting: false,
   dts: true,
-  format: ["cjs"],
+  format: ["esm"],
   clean: true,
   sourcemap: true,
-  external: ["react", "react-dom", "prop-types"],
-  //   esbuildOptions(options, context) {
-  //     options.banner = {
-  //       js: `import * as requireReact from 'react';
-  //        import * as requireReactDom from 'react-dom';
+  external: ["react", "react-dom"],
+  esbuildOptions(options, context) {
+    options.banner = {
+      js: `import * as requireReact from 'react';
+         import * as requireReactDom from 'react-dom';
 
-  //        function require(m) {
-  //          if (m === 'react') return requireReact;
-  //          if (m === 'react-dom') return requireReactDom;
-  //          throw new Error(\`Unknown module \${m}\`);
-  //        }`,
-  //     };
-  //   },
+         function require(m) {
+           if (m === 'react') return requireReact;
+           if (m === 'react-dom') return requireReactDom;
+           throw new Error(\`Unknown module \${m}\`);
+         }`,
+    };
+  },
 });
